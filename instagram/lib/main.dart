ccf8403ad71869fa4a2d397cf0724101e2abaa39
xylon-b <58454151+xylon-b@users.noreply.github.com>
2023-05-23 02:45:15 +0900

    서버 연결함

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import './style.dart' as style;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MaterialApp(theme: style.theme, home: MyApp()));
@@ -7,8 +9,35 @@ void main() {
 
 var a = TextStyle();
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var tab = 0;
+  var list = [1, 2, 3];
+  var map = {'name': 'john', 'age': 20};
+  var data = [];
+
+  getData() async {
+    var result = await http
+        .get(Uri.parse('https://codingapple1.github.io/app/data.json'));
+    var result2 = jsonDecode(result.body);
+    setState(() {
+      data = result2;
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,17 +52,15 @@ class MyApp extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(
-        child: Column(
-          children: [
-            Image.asset('assets/images/jelly_fish.jpg'),
-            Text("lkjlkj"),
-          ],
-        ),
-      ),
+      body: [HomePage(data: data), Text("샵페이지")][tab],
       bottomNavigationBar: BottomNavigationBar(
-          showSelectedLabels: true,
-          showUnselectedLabels: true,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          onTap: (i) {
+            setState(() {
+              tab = i;
+            });
+          },
           items: [
             BottomNavigationBarItem(
                 icon: Icon(Icons.home_outlined), label: 'home'),
@@ -41,5 +68,35 @@ class MyApp extends StatelessWidget {
                 icon: Icon(Icons.shopping_bag_outlined), label: 'shop'),
           ]),
     );
+  }
+}
+
+// 인스타용 서버 : https://codingapple1.github.io/app/data.json
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key, this.data});
+  final data;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 3,
+        itemBuilder: (c, i) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(data[i]['image']),
+              Container(
+                child: Column(
+                  children: [
+                    Text('lkjlkj'),
+                    Text("글쓴이"),
+                    Text("글내용"),
+                  ],
+                ),
+              )
+            ],
+          );
+        });
   }
 }
